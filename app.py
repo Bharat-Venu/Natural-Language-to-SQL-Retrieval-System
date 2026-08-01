1c0e87ec7d0b2ce9b9bf8b2008d349be79d13032
Bharat Venu <bharatvenu8@gmail.com>
2026-08-01 13:28:55 +0530

    changed the all libraries

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
+
+
 import streamlit as st
 import sqlite3
 from urllib.parse import quote_plus
-
 from pathlib import Path
-from langchain.agents import create_sql_agent
-from langchain.sql_database import SQLDatabase
-from langchain.agents.agent_types import AgentType
-from langchain.callbacks import StreamlitCallbackHandler  
-from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+
 from sqlalchemy import create_engine
+
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import (
+    SQLDatabaseToolkit,
+    create_sql_agent,
+)
+
+from langchain.callbacks import StreamlitCallbackHandler
 from langchain_groq import ChatGroq
 
 
